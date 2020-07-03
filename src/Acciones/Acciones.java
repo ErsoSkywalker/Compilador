91627ed881a21471aac212b4c7b8581d1365cad1
Christian Ontiveros <contiverosv@outlook.com>
2020-07-03 16:09:59 -0500

    Compilador para la clase de Carrillo

--- src/Acciones/Acciones.java
+++ src/Acciones/Acciones.java
@@ -1,6 +1,127 @@
 
 package Acciones;
 
+import Entities.InstruccionEntity;
+import java.util.ArrayList;
+
 public class Acciones {
+    public Acciones(ArrayList<InstruccionEntity> InstruccionesBase, ArrayList<InstruccionEntity> InstruccionesACompilar){
+        
+        for(int i = 0; i < InstruccionesACompilar.size(); i++){
+            switch(InstruccionesACompilar.get(i).getInstruccion()){
+                case "%PRINT":{
+                    AccionPrint(InstruccionesACompilar.get(i).getDefinicion());
+                    break;
+                }
+                case "%SUMA":{
+                    AnalisisNumerico(InstruccionesACompilar.get(i).getDefinicion(), 1);
+                    break;
+                }
+                case "%RESTA":{
+                    AnalisisNumerico(InstruccionesACompilar.get(i).getDefinicion(), 2);
+                    break;
+                }
+                case "%MULTI":{
+                    AnalisisNumerico(InstruccionesACompilar.get(i).getDefinicion(), 3);
+                    break;
+                }
+                case "%DIV":{
+                    AnalisisNumerico(InstruccionesACompilar.get(i).getDefinicion(), 4);
+                    break;
+                }
+                case "%EXPO":{
+                    AnalisisNumerico(InstruccionesACompilar.get(i).getDefinicion(), 5);
+                    break;
+                }
+                case "%HELP":{
+                    AccionHelp(InstruccionesACompilar.get(i).getDefinicion(), InstruccionesBase);
+                    break;
+                }
+            }
+        }
+        
+    }
+    
+    public void AccionPrint(String PorImprimir){
+        System.out.println(PorImprimir);
+    }
+    
+    public void AnalisisNumerico(String PorAnalizar, int Accion){
+        int ConteoEspacios = 0, BanderaI = 0;
+        double Resultado = 0;
+        String NumeroUnoSinTratar = "", NumeroDosSinTratar = "";
+        for(int i = 0; i < PorAnalizar.length(); i++ )
+                ConteoEspacios += (PorAnalizar.charAt(i) == ' ')? 1 : 0;
+        
+        if(ConteoEspacios != 1){
+            System.out.println("Oye Bro, La cantidad de parametros no es correcta para esta funcion >:c Recuerda, solo puedes usar dos datos uwu");
+        }else{
+            for(int i = 0; i < PorAnalizar.length(); i++ ){
+                if(PorAnalizar.charAt(i) != ' '){
+                    NumeroUnoSinTratar+= PorAnalizar.charAt(i);
+                }else{
+                    BanderaI = i;
+                    break;
+                }
+            }
+            
+            for(int i = BanderaI+1; i < PorAnalizar.length(); i++){
+                NumeroDosSinTratar+= PorAnalizar.charAt(i);
+            }
+            
+            if(NumeroUnoSinTratar.matches("[0-9]+") && NumeroDosSinTratar.matches("[0-9]+")){
+                
+                switch(Accion){
+                    case 1:{
+                        Resultado = Integer.parseInt(NumeroUnoSinTratar) + Integer.parseInt(NumeroDosSinTratar);
+                        System.out.println(Resultado);
+                        break;
+                    }
+                    case 2:{
+                        Resultado = Integer.parseInt(NumeroUnoSinTratar) - Integer.parseInt(NumeroDosSinTratar);
+                        System.out.println(Resultado);
+                        break;
+                    }
+                    case 3:{
+                        Resultado = Integer.parseInt(NumeroUnoSinTratar) * Integer.parseInt(NumeroDosSinTratar);
+                        System.out.println(Resultado);
+                        break;
+                    }
+                    case 4:{
+                        Resultado = Integer.parseInt(NumeroUnoSinTratar) / Integer.parseInt(NumeroDosSinTratar);
+                        System.out.println(Resultado);
+                        break;
+                    }
+                    case 5:{
+                        Resultado = Math.pow(Integer.parseInt(NumeroUnoSinTratar), Integer.parseInt(NumeroDosSinTratar));
+                        System.out.println(Resultado);
+                        break;
+                    }
+                }
+                
+                
+            }else{
+                System.out.println("Solo se pueden recibir numeros en los parametros :D");
+            }
+            
+        }
+    }
+
+    public void AccionHelp(String Texto, ArrayList<InstruccionEntity> InstruccionesBase){
+        int Validacion = 0;
+        for(int i = 0; i < InstruccionesBase.size(); i++){
+            if(Texto.equalsIgnoreCase(InstruccionesBase.get(i).getInstruccion())){
+                System.out.println("Bienvenido al Modulo de ayuda, la instrucción que buscas es la siguiente");
+                System.out.println(Texto + ": " + InstruccionesBase.get(i).getDefinicion());
+                Validacion++;
+                break;
+            }
+        }
+        if(Validacion == 0){
+            System.out.println("No hemos encontrado tu instrucción en la lista de instrucciones uwu");
+        }
+        
+    }
+    
     
 }
--- src/Logica/Validacion.java
+++ src/Logica/Validacion.java
@@ -1,30 +1,58 @@
 package Logica;
-
+// C:\Users\conti\Desktop\Asesorias\Compilador\Codigos\CodigoACompilar.txt
+import Acciones.Acciones;
 import Entities.InstruccionEntity;
 import java.util.ArrayList;
-
+// Parches futuros= Validar que no hayan Start ni end entre los start y end del inicio
 public class Validacion {
 
     public Validacion(ArrayList<InstruccionEntity> InstruccionesBase, ArrayList<InstruccionEntity> InstruccionesACompilar) {
-        Contrastar(InstruccionesBase, InstruccionesACompilar);
+        int ValidacionErrores = Contrastar(InstruccionesBase, InstruccionesACompilar);
 
+        if(ValidacionErrores == 0){
+            
+            Acciones ac = new Acciones(InstruccionesBase, InstruccionesACompilar);
+            
+        }else{
+            System.out.println("Por favor revisa tu codigo");
+        }
+        
     }
 
-    public void Contrastar(ArrayList<InstruccionEntity> InstruccionesBase, ArrayList<InstruccionEntity> InstruccionesACompilar) {
-        boolean Validador = false;
+    public int Contrastar(ArrayList<InstruccionEntity> InstruccionesBase, ArrayList<InstruccionEntity> InstruccionesACompilar) {
+        boolean Validador = false, Validador2 = false;
+        int CantidadDeErrores = 0; 
+        int CaracterErroneo = 0;
         for (int i = 0; i < InstruccionesACompilar.size(); i++) {
+            //Desde acá checa end y start
             for (int j = 0; j < InstruccionesBase.size(); j++) {
                 Validador = (InstruccionesACompilar.get(i).getInstruccion().equalsIgnoreCase(InstruccionesBase.get(j).getInstruccion())) ? true : Validador;
             }
             if(!Validador){
-                //Aqui identificar donde la cagó
-                System.out.println("Tu Linea uno está mal >:c");
-            }else{
-                System.out.println("Lo lograste compa, linea uno correcta");
+                
+                CantidadDeErrores++;
+                String LineaConError = InstruccionesACompilar.get(i).getInstruccion();
+                for(int j = 1; j <= LineaConError.length(); j++){
+                    Validador2 = false;
+                    for(int k = 0; k < InstruccionesBase.size(); k++){
+                        if(j <= InstruccionesBase.get(k).getInstruccion().length()){
+                            if(LineaConError.substring(0,j).equalsIgnoreCase(InstruccionesBase.get(k).getInstruccion().substring(0, j))){
+                                Validador2 = true;
+                            }
+                        }
+                    }
+                    if(!Validador2){
+                        CaracterErroneo = j-1;
+                        System.out.println("Linea " + i +" Caracter erroneo " + CaracterErroneo);
+                        System.out.println("Posición Especifica marcada con asterisco: " + LineaConError.substring(0, CaracterErroneo) + "*" + LineaConError.substring(CaracterErroneo+1, LineaConError.length()));
+                        break;
+                    }
+                }
+                
             }
             Validador = false;
         }
-
+        return CantidadDeErrores;
     }
 
 }
